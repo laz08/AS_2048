d4747c563980e07cedfc3c19294bf0103e5ad22c
Laura <lazeru08@gmail.com>
2015-06-09 11:49:11 +0200

    Un sol panel

--- src/PresentationLayer/Autentif.java
+++ src/PresentationLayer/Autentif.java
@@ -1,6 +1,8 @@
 package PresentationLayer;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by laura on 09/06/15.
@@ -12,13 +14,29 @@ public class Autentif {
     private JTextField textField1;
     private JPanel PanelAut;
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Autentif");
+    public static void main(JFrame frame) {
+        frame.setTitle("2048 - Autentificació");
         frame.setContentPane(new Autentif().PanelAut);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
         frame.setResizable(false);
-        //frame.pack();
         frame.setVisible(true);
+    }
+
+    public Autentif(){
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textField1.setText("Hola");
+                //Avisa al controller que ha d'anar a nova pantalla: Crear nova partida (Mirar cas d'ús)
+            }
+        });
+        cancelLarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //Tancar tot. Finaltza execució
+                System.exit(0);
+            }
+        });
     }
 }
--- src/PresentationLayer/JugarPartidaView.java
+++ src/PresentationLayer/JugarPartidaView.java
@@ -2,28 +2,22 @@ package PresentationLayer;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class JugarPartidaView extends JFrame implements ActionListener{
+public class JugarPartidaView extends JFrame{
     private Autentif autentifView;
     public JugarPartidaView(){
-        setTitle("Joc 2048");
+        /*setTitle("Joc 2048");
         setBackground(Color.darkGray);
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setVisible(true);
+        */
         prepareAut();
     }
 
     public void prepareAut() {
        autentifView = new Autentif();
-        autentifView.main(null);
+        autentifView.main(this);
     }
-
-    public void actionPerformed(ActionEvent arg0) {
-
-    }
-
 }
