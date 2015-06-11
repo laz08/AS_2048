5975494fac0a2bcdcf2ee8ebe670fb10e90df78c
miquel.xamani <miquel.xamani@est.fib.upc.edu>
2015-06-11 11:35:27 +0200

    corregits errors de compilació

--- src/DomainLayer/MillorPuntuacioMitjana.java
+++ src/DomainLayer/MillorPuntuacioMitjana.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
  * Created by Miquel on 10/06/2015.
  */
 public class MillorPuntuacioMitjana implements RankingStrategy {
-    public Ranking obteRanking(ArrayList<Jugador>){
-
+    public Ranking obteRanking(ArrayList<Jugador> llistaJugadors){
+        Ranking r = new Ranking();
+        return r;
     }
 }
