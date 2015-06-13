44a4a67e66e39462786fba44b469398eee8c2c49
Sergi Orra <sergi.orra.genero@est.fib.upc.edu>
2015-06-13 12:05:45 +0200

    login

--- src/DomainLayer/CtrlCULogin.java
+++ src/DomainLayer/CtrlCULogin.java
@@ -1,15 +1,23 @@
 package DomainLayer;
 
+import java.io.IOException;
+
 /**
  * Created by Miquel on 11/06/2015.
  */
 public class CtrlCULogin {
 
+    private UsuariRegistrat usuariR;
+
     public CtrlCULogin(){
 
     }
 
-    public void Login(String userN, String passwd){
-
+    public void Login(String userN, String passwd) throws IOException{
+        CtrlDataFactoria ctrlDataFactoria = CtrlDataFactoria.getInstance();
+        CtrlUsuari cu = ctrlDataFactoria.getCtrlUsuari();
+        usuariR = cu.getUsuari(userN);
+        String pwd = usuariR.getPwd();
+        if (!passwd.equals(pwd)) throw new IOException("Password Incorrecte");
     }
 }
