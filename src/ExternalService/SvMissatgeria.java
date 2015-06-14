66b40ad10a559fadb97e73d3b864ef79f34382fe
miquel.xamani <miquel.xamani@est.fib.upc.edu>
2015-06-14 16:18:17 +0200

    servei extern fet

--- src/ExternalService/SvMissatgeria.java
+++ src/ExternalService/SvMissatgeria.java
@@ -1,13 +1,19 @@
 package ExternalService;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
 /**
  * Created by Miquel on 13/06/2015.
  */
 public class SvMissatgeria {
+    private Properties p;
     public SvMissatgeria(){}
     public void enviarCorreu(String missatge,String destinatari){
-        String compte = "";
-        String password = "";
+        final String compte = "as2048grup10@gmail.com";
+        final String password = "linuxrulez";
         String assumpte = "Partida jugada al joc 2048";
         String contingut = "La puntuació obtinguda en la partida ";
         boolean trobat = false;
@@ -25,6 +31,33 @@ public class SvMissatgeria {
                 trobat = true;
             }
         }
+
         contingut = contingut + id + "ha estat de " + puntuacio;
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(compte, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(compte));
+            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(destinatari));
+            message.setSubject(assumpte);
+            message.setText(contingut);
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
