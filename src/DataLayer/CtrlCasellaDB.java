a73b8a629721b388b66e0c8a48b302948451d4a3
Sergi Orra <sergi.orra.genero@est.fib.upc.edu>
2015-06-16 02:29:54 +0200

    afe

--- src/DataLayer/CtrlCasellaDB.java
+++ src/DataLayer/CtrlCasellaDB.java
@@ -1,7 +1,47 @@
 package DataLayer;
 
+import DomainLayer.DomainModel.CasellaPK;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
 /**
  * Created by SERGI on 16/06/2015.
  */
 public class CtrlCasellaDB {
+
+    public CtrlCasellaDB() {}
+
+    public CasellaPK get(int idPartida, int numFila, int numCol) throws Exception
+    {
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session session = sf.openSession();
+        CasellaPK c = (CasellaPK) session.get(CasellaPK.class, idPartida, numFila, numCol);
+        if(c == null)
+            throw new Exception("No existeix cap Casella amb aquests parametres");
+        session.close();
+        return c;
+    }
+
+    public boolean exists(int idPartida, int numFila, int numCol)
+    {
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session session = sf.openSession();
+        CasellaPK c = (CasellaPK) session.get(CasellaPK.class, idPartida, numFila, numCol);
+        session.close();
+        if(c == null)
+            return false;
+        return true;
+    }
+
+    public List<CasellaPK> all(){
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session session = sf.openSession();
+        List<CasellaPK> caselles = session.createCriteria(CasellaPK.class).list();
+        session.close();
+        return caselles;
+    }
+
 }
